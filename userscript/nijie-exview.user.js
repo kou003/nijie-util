417dbf615c026d9f5d29c2214f9be784e94191a2
kou003 <kou.000333@gmail.com>
2021-06-18 10:33:04 +0900

    fix

--- userscript/nijie-exview.user.js
+++ userscript/nijie-exview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nijie-exview
 // @namespace    https://github.com/kou003/
-// @version      1.2
+// @version      1.4
 // @description  nijie-exview
 // @author       kou003
 // @match        https://sp.nijie.info/view.php?id=*
@@ -18,20 +18,30 @@
     #exView {
       display: none;
     }
-    .ex-open, #exView:checked~.ex-close {
+    .ex-open {
       display: block;
     }
-    #exView:checked~.popup_illust {
+    .ex-close,
+    .popup_illust {
+      display: none;
+    }
+    #exView:checked~#illust .ex-close {
+      display: block;
+    }
+    #exView:checked ~ #illust .popup_illust {
       display: inline;
     }
-    #exView:checked~.ex-open, .ex-close, .popup_illust {
+    #exView:checked ~ div:not(#illust),
+    #exView:checked ~ #illust a,
+    #exView:checked ~ #illust .ex-open {
       display: none;
     }`;
-    const illust = document.querySelector('#illust');
-    illust.insertAdjacentHTML('beforeend', '<input id="exView" type="checkbox">');
+    const viewCenter = document.querySelector('#view-center-block');
+    viewCenter.insertAdjacentHTML('afterbegin', '<input id="exView" type="checkbox">');
+    const illust = viewCenter.querySelector('#illust');
     illust.insertAdjacentHTML('beforeend', '<label class="ex-open" for="exView"><i class="fa fa-angle-down"></i><label>');
     illust.insertAdjacentHTML('beforeend', '<label class="ex-close" for="exView"><i class="fa fa-angle-up"></i><label>');
-    const exView = illust.querySelector('#exView');
+    const exView = viewCenter.querySelector('#exView');
     const exClose = illust.querySelector('.ex-close');
     const url = location.href.replace('view.php', 'view_popup.php');
     const text = await fetch(url).then(r => r.text());
@@ -44,7 +54,6 @@
       }, false);
       illust.appendChild(exClose.cloneNode(true));
     });
-    imgs[0].scrollIntoView();
     exView.onchange = async e => (exView.checked ? imgs[0] : illust).scrollIntoView();
   }
   if (document.readyState == 'loading') {
