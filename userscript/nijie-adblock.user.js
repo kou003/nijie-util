fc84802572c35dbb334f5960c9d10b9b9bf25ee6
kou003 <kou.000333@gmail.com>
2021-06-18 09:59:51 +0900

    add

---
+++ userscript/nijie-adblock.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         nijie-adblock
+// @namespace    https://github.com/kou003/
+// @version      1.0
+// @description  nijie-exview
+// @author       kou003
+// @match        https://sp.nijie.info/*
+// @updateURL    https://github.com/kou003/nijie-util/raw/master/userscript/nijie-adblock.user.js
+// @downloadURL  https://github.com/kou003/nijie-util/raw/master/userscript/nijie-adblock.user.js
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+{
+  'use strict';
+  const main = () => {
+    document.head.appendChild(document.createElement('style')).textContent = '#inter_background,#header_ad,#center_ad,#footer_ad,#view_header_ad,#sp_view_popup_header{display:none}';
+  }
+  if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', main);
+  } else {
+    main();
+  }
+}
