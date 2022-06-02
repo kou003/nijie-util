2c3584dc77914897479823f6d651d7c138c03eea
kou003 <kou.000333@gmail.com>
2022-06-03 03:11:22 +0900

    [update]header lock

--- userscript/nijie-adblock.user.js
+++ userscript/nijie-adblock.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nijie-adblock
 // @namespace    https://github.com/kou003/
-// @version      1.4
+// @version      1.4.1
 // @description  nijie-adblock
 // @author       kou003
 // @match        https://sp.nijie.info/*
@@ -13,7 +13,7 @@
 {
   'use strict';
   const main = () => {
-    document.head.appendChild(document.createElement('style')).textContent = '#inter_background,#header_ad,#center_ad,#footer_ad,#view_header_ad,#sp_view_popup_header{display:none}';
+    document.head.appendChild(document.createElement('style')).textContent = '#inter_background,#header_ad,#center_ad,#footer_ad,#view_header_ad,#sp_view_popup_header{display:none}header{position:unset}';
   }
   if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', main);
