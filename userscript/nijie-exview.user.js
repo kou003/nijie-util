b5c7286080653b881e81d71bb7e28a27a56d52f8
kou003 <kou.000333@gmail.com>
2022-10-05 21:56:45 +0900

    ver_up

--- userscript/nijie-exview.user.js
+++ userscript/nijie-exview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nijie-exview
 // @namespace    https://github.com/kou003/
-// @version      3.13.2
+// @version      3.13.3
 // @description  nijie-exview
 // @author       kou003
 // @match        https://sp.nijie.info/view.php?id=*
